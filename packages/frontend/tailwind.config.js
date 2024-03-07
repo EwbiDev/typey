94bdfc02d0277ce7422d6d85b1e39f805ae9ba82
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-07 12:01:40 +0000

    feat: setup site colors

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     extend: {
       fontFamily: {
         solway: ["solway", "serif"]
+      },
+      colors: {
+        "typey-background": "#303025",
+        "typey-primary": "#A18CCF",
+        "typey-secondary": "#75AAA1",
+        "typey-default": "#EAEAEA",
+        "typey-ok": "#88EE57",
+        "typey-bad": "#F16647"
       }
     },
   },
