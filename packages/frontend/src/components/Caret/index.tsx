289f30631b4fc81e076498a6b2a596dd78168004
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-10 16:46:23 +0000

    feat: resize caret

--- packages/frontend/src/components/Caret/index.tsx
+++ packages/frontend/src/components/Caret/index.tsx
@@ -2,7 +2,7 @@ export default function Caret({ loc }: Passage.Prop.Caret) {
   return (
     <>
       <div
-        className={`absolute h-6 border`}
+        className={`absolute h-5 border`}
         style={{ top: `${loc.y}px`, left: `${loc.x}px` }}
       ></div>
     </>
