6089ee563561ab2afa5e22ddad57d623ec5fb0d1
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-14 18:03:55 +0000

    feat: add home reroute to passage

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,18 +1,26 @@
 import PageHeader from "./components/PageHeader";
 import PassageContainer from "./components/PassageContainer";
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <div className="bg-typey-background text-typey-default flex h-screen w-screen items-center justify-center">
+      <div className="flex h-screen w-screen items-center justify-center bg-typey-background text-typey-default">
         <div className="container mx-8 flex h-full w-full flex-col">
           <PageHeader />
           <Routes>
-            <Route path="/passage/:passageId" element={<PassageContainer />}></Route>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/passage/:passageId"
+              element={<PassageContainer />}
+            ></Route>
           </Routes>
         </div>
       </div>
     </>
   );
+}
+
+function Home() {
+  return <Navigate to={"/passage/1"} />;
 }
