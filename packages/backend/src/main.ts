39acf2f322b2c5d837476f604e7d6bd570a04046
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-14 10:54:15 +0000

    feat: enable cors

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -6,6 +6,7 @@ const { SERVER_PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api/v1');
   await app.listen(SERVER_PORT);
 }
