3ee9a61c0ffdd3807f915cef57b8d3855f49a361
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-15 12:02:44 +0000

    chore: update seed

--- packages/backend/prisma/seed.ts
+++ packages/backend/prisma/seed.ts
@@ -4,14 +4,15 @@ const prisma = new PrismaClient();
 async function main() {
   const texts = [
     'Welcome to Typey!',
-    'If I get things right, it all appears green.',
     'I can navigate backwards, forwards, and reset.',
+    'Can easily link to a passage!',
+    'If I get things right, it all appears green.',
     'If things go wrong, it highlights red.',
     'If I get it wrong, and not notice, I can no longer type.',
     'Emojis 👏 also 👏 work',
-    'Can easily link to a passage!',
     'What am I missing?',
-    'No users, no user passage submission, no high scores, no keyboard shortcuts',
+    'No users, no user passage submission, no high scores, no keyboard shortcuts.',
+    'life without call course know never old head one good since off develop no present large off interest before want from you fact ask help move',
   ];
 
   const passages = await prisma.passage.createMany({
