dff8078f5c0357477721ad4699ac47dafd4d082e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-15 08:59:52 +0000

    style: formatting

--- packages/frontend/src/components/PassageControls/index.tsx
+++ packages/frontend/src/components/PassageControls/index.tsx
@@ -1,6 +1,8 @@
 import ControlIcon from "./ControlIcon";
 
-export default function PassageControls({ replayPassage }) {
+export default function PassageControls({
+  replayPassage,
+}: Passage.Prop.Controls) {
   return (
     <div className="flex w-6/12 select-none justify-around place-self-center rounded-2xl bg-typey-default p-4 text-xl text-typey-primary">
       <ControlIcon iconName="replay" onClick={replayPassage} />
