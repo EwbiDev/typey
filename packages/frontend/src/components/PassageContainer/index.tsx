af202c7e4eabc05b7d651df49a3cd466fb88366e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-06-24 17:30:33 +0100

    feat: add passage not found handling

--- packages/frontend/src/components/PassageContainer/index.tsx
+++ packages/frontend/src/components/PassageContainer/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
+import Button from "../Button";
 import PassageInput from "../PassageInput";
 import PassageStatDisplay from "../PassageStatDisplay";
 import PassageControls from "../PassageControls";
@@ -16,6 +17,7 @@ export default function PassageContainer() {
   const [passageStats, setPassageStats] =
     useState<Passage.Stats>(setupPassageStats());
   const [wordIndex, setWordIndex] = useState(0);
+  const [passageFound, setPassageFound] = useState(true);
 
   const navigate = useNavigate();
 
@@ -24,12 +26,17 @@ export default function PassageContainer() {
   useEffect(() => {
     async function newPassageText() {
       const data = await passageApi.getById(Number(passageId));
-      if (data.statusText === "OK") {
+
+      if (data && data.statusText === "OK") {
         const newPassage = setupPassage(data.data.text);
         setPassage(newPassage);
         setPassageStats(setupPassageStats);
         setWordIndex(0);
+        setPassageFound(true);
+        return;
       }
+
+      setPassageFound(false);
     }
     newPassageText();
   }, [passageId]);
@@ -56,7 +63,8 @@ export default function PassageContainer() {
 
   return (
     <div className="flex grow flex-col justify-center gap-8">
-      {!passageComplete && (
+      {!passageFound && <PassageNotFound />}
+      {!passageComplete && passageFound && (
         <PassageInput
           passage={passage}
           passageComplete={passageComplete}
@@ -67,7 +75,7 @@ export default function PassageContainer() {
           setWordIndex={setWordIndex}
         />
       )}
-      {passageComplete && (
+      {passageComplete && passageFound && (
         <PassageStatDisplay
           nextPassage={nextPassage}
           passage={passage}
@@ -118,4 +126,69 @@ function clearPassageAttempt(passage: Passage.Word[]): Passage.Word[] {
 
 function setupPassageStats() {
   return { startTime: 0, endTime: 0, accuracy: { hit: 0, miss: 0, extras: 0 } };
+}
+
+function PassageNotFound() {
+  const [showInput, setShowInput] = useState(false);
+  const [passageInput, setPassageInput] = useState("");
+  const [buttonText, setButtonText] = useState("Submit");
+  const [submitted, setSubmitted] = useState(false);
+  const [newPassageId, setNewPassageId] = useState<number>();
+
+  const navigate = useNavigate();
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassageInput(event.target.value);
+  }
+
+  async function handleButton() {
+    if (submitted) {
+      navigate(`/passage/${newPassageId}`);
+    }
+
+    if (passageInput.length < 6) {
+      setButtonText("Error!");
+      return;
+    }
+
+    const newPassage = await passageApi.post(passageInput);
+    if (newPassage.statusText === "Created") {
+      setButtonText("Created!");
+      setSubmitted(true);
+      setNewPassageId(newPassage.data.id);
+    }
+  }
+
+  return (
+    <div className="relative flex min-h-24 items-center justify-center gap-8">
+      {!showInput && (
+        <>
+          <div>Passage Not Found!</div>
+          <Button
+            onClick={() => {
+              setShowInput(true);
+            }}
+            text="Add a new passage?"
+            type="primaryEmpty"
+          />
+        </>
+      )}
+      {showInput && (
+        <div className="flex gap-8">
+          <input
+            name="passage-input"
+            id="passage-input"
+            value={passageInput}
+            onChange={handleInputChange}
+            className="rounded-sm bg-typey-background p-4 outline outline-typey-secondary"
+          />
+          <Button
+            onClick={handleButton}
+            text={buttonText}
+            type="secondaryFull"
+          />
+        </div>
+      )}
+    </div>
+  );
 }
