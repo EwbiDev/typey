4cc894daf78c4a2279665c21b5cbecb7025fd3c6
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-06-26 17:41:12 +0100

    feat: change to add icon

--- packages/frontend/src/components/PassageControls/index.tsx
+++ packages/frontend/src/components/PassageControls/index.tsx
@@ -8,7 +8,7 @@ export default function PassageControls({
   return (
     <div className="flex w-6/12 select-none justify-around place-self-center rounded-2xl bg-typey-default p-4 text-xl text-typey-primary">
       <ControlIcon iconName="replay" onClick={replayPassage} />
-      <ControlIcon iconName="trophy" onClick={() => {}} />
+      <ControlIcon iconName="add" onClick={() => {}} />
       <ControlIcon iconName="arrow_left_alt" onClick={prevPassage} />
       <ControlIcon iconName="arrow_right_alt" onClick={nextPassage} />
     </div>
