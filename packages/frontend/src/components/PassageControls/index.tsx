77f28b1fd491ca487831d2c569005f478e4a678e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-07-01 14:22:16 +0100

    feat: implement add button functionality

--- packages/frontend/src/components/PassageControls/index.tsx
+++ packages/frontend/src/components/PassageControls/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import ControlIcon from "./ControlIcon";
 
 export default function PassageControls({
@@ -5,10 +7,17 @@ export default function PassageControls({
   nextPassage,
   replayPassage,
 }: Passage.Prop.Controls) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex w-6/12 select-none justify-around place-self-center rounded-2xl bg-typey-default p-4 text-xl text-typey-primary">
       <ControlIcon iconName="replay" onClick={replayPassage} />
-      <ControlIcon iconName="add" onClick={() => {}} />
+      <ControlIcon
+        iconName="add"
+        onClick={() => {
+          navigate("/passage/new");
+        }}
+      />
       <ControlIcon iconName="arrow_left_alt" onClick={prevPassage} />
       <ControlIcon iconName="arrow_right_alt" onClick={nextPassage} />
     </div>
