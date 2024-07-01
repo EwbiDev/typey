793e828021986d05032d96cc4bedeefa6a70076a
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-07-01 15:21:40 +0100

    feat: setup auth and user modules

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassagesModule } from './passages/passages.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PassagesModule, PrismaModule],
+  imports: [PassagesModule, PrismaModule, AuthModule, UsersModule],
 })
 export class AppModule {}
