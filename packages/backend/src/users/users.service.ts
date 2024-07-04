455c4b5d1e1a4f9a987643a01fc0191fb6d62887
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-07-04 14:52:38 +0100

    feat: add return types to UsersService

--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,31 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 import * as bcrypt from 'bcrypt';
 
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
+type UserWithoutInfo = Omit<User, 'passwordHash' | 'createdAt' | 'updatedAt'>;
+
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async create(userData: CreateUserDto) {
+  async create(userData: CreateUserDto): Promise<UserWithoutInfo> {
     const passwordHash = await bcrypt.hash(userData.password, 12);
 
-    const newUser = await this.prisma.user.create({
+    return await this.prisma.user.create({
       data: {
         email: userData.email,
         passwordHash,
       },
+      select: {
+        id: true,
+        email: true,
+      },
     });
-
-    delete newUser.passwordHash;
-    return newUser;
   }
 
-  findByEmail(email: string) {
+  findByEmail(email: string): Promise<UserWithoutInfo> {
     return this.prisma.user.findUnique({
       where: { email },
+      select: {
+        id: true,
+        email: true,
+      },
+    });
+  }
+
+  findById(id: number): Promise<UserWithoutInfo> {
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+      },
     });
   }
 }
