22b9b8543908bd9f72e9028f839df84f01e00f73
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-07-15 15:49:33 +0100

    feat: add response error handling

--- packages/frontend/src/components/RegistrationForm/index.tsx
+++ packages/frontend/src/components/RegistrationForm/index.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 import Input from "../Input";
 import { User } from "../../types/types";
 import { userApi } from "../../utils/api";
+
+interface ErrorDetails {
+  message: string;
+  error: string;
+  statusCode?: number;
+}
 
 export default function RegistrationForm() {
   const {
@@ -10,44 +18,80 @@ export default function RegistrationForm() {
     handleSubmit,
     formState: { errors },
   } = useForm<User.RegistrationFormData>();
+  const [registrationSuccess, setRegistrationSuccess] = useState<boolean>();
+  const [errorDetails, setErrorDetails] = useState<ErrorDetails>();
 
   const onSubmit: SubmitHandler<User.RegistrationFormData> = async (data) => {
     const response = await userApi.register(data);
-    console.log(response);
+
+    if (response && response?.status >= 201 && response?.status < 400) {
+      setRegistrationSuccess(true);
+      return;
+    }
+
+    if (response?.data?.error) {
+      setErrorDetails(response.data);
+      setRegistrationSuccess(false);
+      return;
+    }
+
+    setErrorDetails({
+      message: "Unable to connect to server, please try again later",
+      error: "No connection",
+    });
+    setRegistrationSuccess(false);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>Register</h2>
-      <Input
-        inputType="text"
-        label="username"
-        placeholder="Typey McTypeFace"
-        register={register}
-        minLength={1}
-        maxLength={24}
-        required
-      />
-      <Input
-        inputType="email"
-        label="email"
-        placeholder="typey@mctype.face"
-        register={register}
-        required
-      />
-      <Input
-        inputType="password"
-        label="password"
-        placeholder=""
-        register={register}
-        minLength={8}
-        maxLength={128}
-        required
-      />
-      <input type="submit" />
-      {errors.email && <div>Email error</div>}
-      {errors.username && <div>Username error</div>}
-      {errors.password && <div>Password error</div>}
-    </form>
+    <>
+      {!registrationSuccess && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Register</h2>
+          <Input
+            inputType="text"
+            label="username"
+            placeholder="Typey McTypeFace"
+            register={register}
+            minLength={1}
+            maxLength={24}
+            required
+          />
+          <Input
+            inputType="password"
+            label="password"
+            placeholder=""
+            register={register}
+            minLength={8}
+            maxLength={128}
+            required
+          />
+          <input type="submit" />
+          {errors.username && <div>Username error</div>}
+          {errors.password && <div>Password error</div>}
+        </form>
+      )}
+      {registrationSuccess && <SuccessMessage />}
+      {!registrationSuccess && errorDetails && (
+        <FailureMessage message={errorDetails.message} />
+      )}
+    </>
+  );
+}
+
+function FailureMessage({ message }: { message: string }) {
+  return (
+    <div>
+      <h2>Error:</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+function SuccessMessage() {
+  return (
+    <div>
+      <h2>Success!</h2>
+      <Link to="/login">Login here</Link>
+    </div>
   );
 }
