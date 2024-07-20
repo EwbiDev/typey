0c98b3178b406b91147b38ae8ba5526f9525da3a
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-07-20 16:44:43 +0100

    refactor: add prop type

--- packages/frontend/src/components/Container/index.tsx
+++ packages/frontend/src/components/Container/index.tsx
@@ -1,4 +1,4 @@
-export default function Container({ children }) {
+export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex grow flex-col items-center justify-center gap-8">
       {children}
