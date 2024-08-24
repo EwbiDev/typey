3afca21b2e72bdc1e274894b9bfddce15ededf9a
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-08-24 16:29:26 +0100

    build: install tailwind/forms

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from "@tailwindcss/forms"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -18,5 +20,5 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [forms],
 }
