cd69fe8d4e750dfcb89cc5d12e8193b36f00632a
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-08-25 14:56:59 +0100

    refactor: extract NavItem

--- packages/frontend/src/components/PageHeader/NavBar.tsx
+++ packages/frontend/src/components/PageHeader/NavBar.tsx
@@ -1,25 +1,16 @@
-import { DisclosureButton } from "@headlessui/react";
-import { classNames } from "../../utils/classNames";
-
 import { Navigation } from "../../types/types";
+import { NavItem } from "./NavItem";
 
 export function NavBarDesktop({ navigationLinks }: Navigation.Prop.NavBar) {
   return (
     <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
       {navigationLinks.map((item) => (
-        <a
+        <NavItem
           key={item.name}
+          name={item.name}
           href={item.href}
-          aria-current={item.current ? "page" : undefined}
-          className={classNames(
-            item.current
-              ? "bg-typey-primary text-typey-default"
-              : "text-typey-primary hover:bg-typey-primary-light hover:text-white",
-            "rounded-md px-3 py-2 text-sm font-medium",
-          )}
-        >
-          {item.name}
-        </a>
+          current={item.current}
+        />
       ))}
     </div>
   );
@@ -29,20 +20,12 @@ export function NavBarMobile({ navigationLinks }: Navigation.Prop.NavBar) {
   return (
     <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
       {navigationLinks.map((item) => (
-        <DisclosureButton
+        <NavItem
           key={item.name}
-          as="a"
+          name={item.name}
           href={item.href}
-          aria-current={item.current ? "page" : undefined}
-          className={classNames(
-            item.current
-              ? "bg-typey-primary text-typey-default"
-              : "text-typey-primary hover:bg-typey-primary-light hover:text-white",
-            "block rounded-md px-3 py-2 text-base font-medium",
-          )}
-        >
-          {item.name}
-        </DisclosureButton>
+          current={item.current}
+        />
       ))}
     </div>
   );
---
+++ packages/frontend/src/components/PageHeader/NavItem.tsx
@@ -0,0 +1,21 @@
+import { DisclosureButton } from "@headlessui/react";
+import { classNames } from "../../utils/classNames";
+import { Navigation } from "../../types/types";
+
+export function NavItem({ href, current, name }: Navigation.NavigationLink) {
+  return (
+    <DisclosureButton
+      as="a"
+      href={href}
+      aria-current={current ? "page" : undefined}
+      className={classNames(
+        current
+          ? "bg-typey-primary text-typey-default"
+          : "text-typey-primary hover:bg-typey-primary-light hover:text-white",
+        "block rounded-md px-3 py-2 text-base font-medium",
+      )}
+    >
+      {name}
+    </DisclosureButton>
+  );
+}
