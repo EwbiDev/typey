91544e57052d560bc47594ba37bfea46b6eceb7e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-09-01 15:52:32 +0100

    feat: change jwt timeout to 10 mins (for now)

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '600s' },
     }),
   ],
   exports: [AuthService],
